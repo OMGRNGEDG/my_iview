c812d25d2e0c0ae53d8b1b10c6278db1ad1d16ba
wenjunzhang <17621682391@163.com>
2019-12-24 16:24:21 +0800

    加密信息

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,7 +1,50 @@
-import Cookies from 'js-cookie';
+import Cookies from "js-cookie";
+import CryptoJS from "crypto-js";
+
+function getAesString(data, key, iv) {
+  //加密
+  var nowkey = CryptoJS.enc.Utf8.parse(key);
+  var nowiv = CryptoJS.enc.Utf8.parse(iv);
+  var encrypted = CryptoJS.AES.encrypt(data, nowkey, {
+    iv: nowiv,
+    mode: CryptoJS.mode.CBC,
+    padding: CryptoJS.pad.Pkcs7
+  });
+  return encrypted.toString(); //返回的是base64格式的密文
+}
+function getDAesString(encrypted, key, iv) {
+  //解密
+  var nowkey = CryptoJS.enc.Utf8.parse(key);
+  var nowiv = CryptoJS.enc.Utf8.parse(iv);
+  var decrypted = CryptoJS.AES.decrypt(encrypted, nowkey, {
+    iv: nowiv,
+    mode: CryptoJS.mode.CBC,
+    padding: CryptoJS.pad.Pkcs7
+  });
+  return decrypted.toString(CryptoJS.enc.Utf8);
+}
+export function encryption(data) {
+  //加密
+  var encrypted = getAesString(
+    data,
+    "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+    "1234567812345678"
+  ); //密文
+  return encrypted;
+}
+
+export function decrypt(data) {
+  //解密
+  var decryptedStr = getDAesString(
+    data,
+    "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+    "1234567812345678"
+  );
+  return decryptedStr;
+}
 
 export const setToken = (token, tokenName = "token") => {
   Cookies.set(tokenName, token);
-}
+};
 
-export const getToken = (tokenName = "token") => Cookies.get(tokenName)
+export const getToken = (tokenName = "token") => Cookies.get(tokenName);
